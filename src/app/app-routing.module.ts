a2cd1e5a22906856468c316a01c6565022190947
Viveka Hegade <viveka.hegade@epicor.com>
2018-08-06 12:44:39 +0530

    flexbox

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FlexboxComponent } from './components/flexbox/flexbox.component';
 
 const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'flex',
+        component: FlexboxComponent
     }
 ];
 
